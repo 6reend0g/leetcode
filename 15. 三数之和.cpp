98f07f36b638e1b3f4c6d2f6e0144324a9bc2c16
自信小汉堡子 <njtech202021147140@qq.com>
2023-09-13 13:36:23 +0800

    modified:   "15. \344\270\211\346\225\260\344\271\213\345\222\214.cpp"

--- 15. 三数之和.cpp
+++ 15. 三数之和.cpp
@@ -4,6 +4,18 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            for (size_t j = i + 1; j < nums.size(); j++)
+            {
+                for (size_t k = j + 1; k < nums.size(); k++)
+                {
+                   
+                }
+                
+            }
+            
+        }
         
     }
 };
